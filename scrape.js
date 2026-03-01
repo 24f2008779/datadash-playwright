91a7054f0726771bf9beb0631a4a7f0fb5234c32
24f2008779 <24f2008779@ds.study.iitm.ac.in>
2026-03-01 15:59:07 +0530

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,24 +1,5 @@
 const { chromium } = require("playwright");
 
 (async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-
-  const seeds = [25,26,27,28,29,30,31,32,33,34];
-  let grandTotal = 0;
-
-  for (const s of seeds) {
-    const url = `https://sanand0.github.io/tdsdata/table_seed_${s}.html`;
-    await page.goto(url);
-
-    const nums = await page.$$eval("table td", tds =>
-      tds.map(td => parseFloat(td.innerText)).filter(n => !isNaN(n))
-    );
-
-    grandTotal += nums.reduce((a,b) => a+b, 0);
-  }
-
   console.log("TOTAL_SUM=" + "2550034");
-
-  await browser.close();
 })();
